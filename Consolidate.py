d2f429808981d7d5f1f8751456ffeedbda864994
mjs227 <92041658+mjs227@users.noreply.github.com>
2021-12-05 19:54:31 -0500

    Merge tweets.json with stance detection file

---
+++ Consolidate.py
@@ -0,0 +1,34 @@
+import json
+
+fp = 'C:/Users/mjsul/Desktop/'
+
+
+if __name__ == "__main__":
+    #put the original tweets.json here
+    with open(fp + 'tweets.json') as f:
+        data_tr = json.load(f)
+
+    #put tweets with stance detection here
+    with open(fp + 'tweets_VaxxTopics.json') as f2:
+        data_ling = json.load(f2)
+
+    index_tr = {}
+    index_ling = {}
+    out = []
+
+    for d in data_tr:
+        index_tr.update({d['id']: d})
+
+    for d in data_ling:
+        index_ling.update({d['id']: d})
+
+    for k in index_tr.keys():
+        if k in index_ling.keys():
+            index_tr[k].update({
+                'stance': index_ling[k]['stance'],
+            })
+
+        out.append(index_tr[k])
+
+    with open(fp + 'tweets.json', 'w') as f3:
+        json.dump(out, f3)
